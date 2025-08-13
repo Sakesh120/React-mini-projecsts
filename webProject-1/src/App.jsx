338db4bab68bbf785df87c759983fd697827e59b
Sakesh120 <sakeshchopkar927@gmail.com>
2025-08-13 22:49:31 +0530

    new webproject

---
+++ webProject-1/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-5xl mt-4 mx-2  bg-green-400'>hey this is a SAKESH CHOPKAR</h1>
+    </div>
+  )
+}
+
+export default App
