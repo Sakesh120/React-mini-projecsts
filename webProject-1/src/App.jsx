f00c6236cf7de7c1a409fa58db28178b06678e48
Sakesh120 <sakeshchopkar927@gmail.com>
2025-08-15 11:55:16 +0530

    update navbar

--- webProject-1/src/App.jsx
+++ webProject-1/src/App.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Navbar from './components/navbar'
 
 const App = () => {
+const [theme,setTheme]=useState('light')
+
   return (
-    <div>
-      <h1 className='text-5xl mt-4 mx-2  bg-green-400'>hey this is a SAKESH CHOPKAR</h1>
+    <div className='dark:bg-black relative '> 
+      <Navbar theme={theme} setTheme={setTheme}/>
     </div>
   )
 }
