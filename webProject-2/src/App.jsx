85059c290c3cd243fc0f963a11f30b5220beee55
Sakesh120 <sakeshchopkar927@gmail.com>
2025-08-19 23:16:16 +0530

    add programs sec

--- webProject-2/src/App.jsx
+++ webProject-2/src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero/Hero'
+import Programs from './components/programs/Programs'
 
 const App = () => {
   return (
     <div>
       <Navbar/>
       <Hero/>
+      <div className="contaiber">
+      <Programs/>
+      </div>
       
     </div>
   )
