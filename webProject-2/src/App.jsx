1c48794b26d1b0d9b7afbf9171b51ba2d0120cbd
Sakesh120 <sakeshchopkar927@gmail.com>
2025-08-20 21:48:26 +0530

    add new compo

--- webProject-2/src/App.jsx
+++ webProject-2/src/App.jsx
@@ -1,17 +1,28 @@
 import React from 'react'
-import Navbar from './components/Navbar'
+import Navbar from './components/navbar/Navbar'
 import Hero from './components/Hero/Hero'
 import Programs from './components/programs/Programs'
+import Title from './components/title/Title'
+import About from './components/about/About'
+import Campus from './components/campus/Campus'
+import Testimonials from './components/testimonials/Testimonials'
 
 const App = () => {
   return (
     <div>
-      <Navbar/>
-      <Hero/>
-      <div className="contaiber">
-      <Programs/>
+      <Navbar />
+      <Hero />
+      <div className="container">
+        <Title subtitle='our program' title='What We Offer' />
+        <Programs />
+        <About />
+        <Title subtitle='Gallery' title='Campus Photos' />
+        <Campus/>
+       <Title subtitle='TESTIMONALS' title='What Student Says' />
+        <Testimonials/>
+
       </div>
-      
+
     </div>
   )
 }
