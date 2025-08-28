79c4dcc93ae48998fdf0b1d7e04b56ee196b3f88
Sakesh120 <sakeshchopkar927@gmail.com>
2025-08-28 22:52:29 +0530

    starting a new clone project

---
+++ Swiggy-Clone/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './header'
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <h1 className="text-[20px] text-red-600 bg-yellow-500">
+        Hello this is vite + React!
+      </h1>
+    </div>
+  )
+}
+
+export default App
