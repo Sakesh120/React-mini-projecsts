f28670b1cfc6f1f83ecc2b4d89e36366938f9c7e
Sakesh120 <sakeshchopkar927@gmail.com>
2025-09-28 23:46:51 +0530

    add category

---
+++ Swiggy-Clone/src/components/Category.jsx
@@ -0,0 +1,116 @@
+import React, { useState } from "react";
+import { GoArrowLeft } from "react-icons/go";
+import { GoArrowRight } from "react-icons/go";
+
+const Category = () => {
+  const [slide, setSlide] = useState(0);
+  const Categories = [
+    {
+      img: "/images/Dosa.jpeg",
+      name: "Dosa",
+    },
+    {
+      img: "/images/Pizza.jpeg",
+      name: "Pizza",
+    },
+    {
+      img: "/images/Pasta.jpeg",
+      name: "Pasta",
+    },
+    {
+      img: "/images/Poori.jpeg",
+      name: "Poori",
+    },
+    {
+      img: "/images/Rolls.jpeg",
+      name: "Rolls",
+    },
+    {
+      img: "/images/Salad.jpeg",
+      name: "salad",
+    },
+    {
+      img: "/images/Paratha.jpeg",
+      name: "Paratha",
+    },
+    {
+      img: "/images/Noodles.jpeg",
+      name: "Noodles",
+    },
+    {
+      img: "/images/Khichdi.jpeg",
+      name: "Khichdi",
+    },
+    {
+      img: "/images/Ice_Creams.jpeg",
+      name: "Ice Cream",
+    },
+    {
+      img: "/images/Gulab_Jamun.jpeg",
+      name: "Gulab Jamun",
+    },
+    {
+      img: "/images/Cakes.jpeg",
+      name: "Cake",
+    },
+    {
+      img: "/images/Burger.jpeg",
+      name: "Burger",
+    },
+    {
+      img: "/images/Chinese.jpeg",
+      name: "Chinese",
+    },
+    {
+      img: "/images/Biryani_2.jpeg",
+      name: "Biryani",
+    },
+    {
+      img: "/images/Chole_Bature.jpeg",
+      name: "Chole Bature",
+    },
+  ];
+  return (
+    <div className="lg:max-w-[1200px] mx-auto ">
+      <div className="flex justify-between items-center  mt-4">
+        <div className="text-2xl font-bold">What's on your mind?</div>
+        <div className="flex justify-center gap-3 items-center [&>*]:cursor-pointer [&>*]:active:bg-[#cabdbd] ">
+          <div
+            className="bg-[#e2e2e2] w-[30px] h-[30px] rounded-full flex justify-center items-center "
+            onClick={() => {
+              if (slide == 0) return false;
+              setSlide(slide - 2);
+            }}
+          >
+            <GoArrowLeft />
+          </div>
+          <div
+            className="bg-[#e2e2e2]  w-[30px] h-[30px] rounded-full flex justify-center items-center"
+            onClick={() => {
+              if (Categories.length - 8 == slide) return false;
+              setSlide(slide + 2);
+            }}
+          >
+            <GoArrowRight />
+          </div>
+        </div>
+      </div>
+      <div className="flex overflow-hidden ">
+        {Categories.map((item, index) => (
+          <div
+            style={{
+              transform: `translateX(-${slide * 100}%)`,
+            }}
+            key={index}
+            className="w-[150px] shrink-0 duration-500"
+          >
+            <img src={item.img} alt={item.name} />
+          </div>
+        ))}
+      </div>
+      <hr className="my-4 border-[1px] border-b-rose-500" />
+    </div>
+  );
+};
+
+export default Category;
--- Swiggy-Clone/src/components/Header.jsx
+++ Swiggy-Clone/src/components/Header.jsx
@@ -1,5 +1,10 @@
 import React, { useState } from "react";
 import { RxCaretDown } from "react-icons/rx";
+import { IoMdSearch } from "react-icons/io";
+import { CiDiscount1 } from "react-icons/ci";
+import { FaHandsHelping } from "react-icons/fa";
+import { FaSignInAlt } from "react-icons/fa";
+import { FaOpencart } from "react-icons/fa";
 
 const Header = () => {
   const [toggle, setToggle] = useState(false);
@@ -7,18 +12,55 @@ const Header = () => {
   const showSideMenu = () => {
     setToggle(true);
   };
+  const hideSideMenu = () => {
+    setToggle(false);
+  };
+
+  const links = [
+    {
+      icon: <IoMdSearch />,
+      name: "Search",
+    },
+    {
+      icon: <CiDiscount1 />,
+      name: "Offers",
+      sup: "new",
+    },
+    {
+      icon: <FaHandsHelping />,
+      name: "Help",
+    },
+    {
+      icon: <FaSignInAlt />,
+      name: "Sign In",
+    },
+    {
+      icon: <FaOpencart />,
+      name: "Cart",
+    },
+  ];
 
   return (
     <>
       <div
-        className="black-overlay w-full h-full fixed duration-500"
+        className="black-overlay w-full h-full fixed duration-[500ms]"
+        onClick={hideSideMenu}
         style={{
           opacity: toggle ? 1 : 0,
           visibility: toggle ? "visible" : "hidden",
         }}
       ></div>
+      <div
+        onClick={(e) => {
+          e.stopPropagation();
+        }}
+        className="w-[500px] bg-white h-full absolute duration-[500ms]"
+        style={{
+          left: toggle ? "0%" : "-100%",
+        }}
+      ></div>
       <header className="p-[15px] shadow-xl ">
-        <div className="max-w-[1200px] mx-auto border border-red-400 flex items-center text-[#686b78]">
+        <div className="max-w-[1200px] mx-auto  flex items-center text-[#686b78]">
           <div className="w-[90px]">
             <img src="images/logo1.png" className="w-full" alt="" />
           </div>
@@ -33,6 +75,18 @@ const Header = () => {
               onClick={showSideMenu}
             />
           </div>
+          <nav className="flex list-none gap-8 ml-auto  text-[18px] font-semibold">
+            {links.map((item, index) => (
+              <li
+                className="flex items-center gap-2 cursor-pointer hover:text-[#fc8019] transition-[0.8s] hover:scale-[1.1]"
+                key={index}
+              >
+                {item.icon}
+                {item.name}
+                <sup className="text-amber-400">{item.sup}</sup>
+              </li>
+            ))}
+          </nav>
         </div>
       </header>
     </>
