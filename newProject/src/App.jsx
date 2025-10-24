71147973a95d8787c3d20c4cc848b53d458f651b
Sakesh120 <sakeshchopkar927@gmail.com>
2025-10-24 07:46:17 +0530

    add requirements

--- newProject/src/App.jsx
+++ newProject/src/App.jsx
@@ -1,11 +1,21 @@
 import React from "react";
-import Header from "./components/Header";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Product from "./pages/Product";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Card from "./pages/Card";
 const App = () => {
   return (
-    <div>
-      <Header />
-      <h1 className="text-5xl font-bold">Hello I am Sakesh</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/products" element={<Product />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+        <Route path="/card" element={<Card />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 };
 
---
+++ newProject/src/pages/Product.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Product = () => {
+  return (
+    <div>
+      <h3>Hey this is product page</h3>
+    </div>
+  );
+};
+
+export default Product;
