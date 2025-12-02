a366508dc1e991c6ac050283b188270458e45d8c
Sakesh120 <sakeshchopkar927@gmail.com>
2025-12-02 08:03:40 +0530

    adding images

---
+++ Animated/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './index.css'
+
+const App = () => {
+  return (
+    <>
+<div className='w-full min-h-screen bg-black text-white'>
+<h1 className='text-4xl font-bold'>Hello World</h1>
+</div>
+    </>
+  )
+}
+
+export default App
