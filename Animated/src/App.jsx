27138e758364a313a6667cb514b1780843024ab8
Sakesh120 <sakeshchopkar927@gmail.com>
2025-12-02 22:31:42 +0530

    +_

--- Animated/src/App.jsx
+++ Animated/src/App.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
-import './index.css'
+import React from "react";
+import "./index.css";
+import Canvas from "./Canvas";
+import data from "./Data";
 
 const App = () => {
   return (
     <>
-<div className='w-full min-h-screen bg-black text-white'>
-<h1 className='text-4xl font-bold'>Hello World</h1>
-</div>
+      <div className="w-full min-h-screen bg-black text-white">
+        {data.map((item, index) => {
+          <div key={index}>
+            {item.map((canvasData, index) => {
+              <Canvas key={index} details={canvasData} />;
+            })}
+          </div>;
+        })}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ Animated/src/Canvas.jsx
@@ -0,0 +1,44 @@
+import React, { use, useEffect, useRef, useState } from "react";
+import images from "./image";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
+const Canvas = ({ details }) => {
+  const { startIndex, numImages, duration, size, top, left, zIndex } = details;
+  const [index, setIndex] = useState({ value: startIndex });
+  const canvasRef = useRef(null);
+
+  useGSAP(() => {
+    gsap.to(index, {
+      value: startIndex + numImages - 1,
+      duration: duration,
+      repeat: -1,
+      ease: "linear",
+      onUpdate: () => {
+        setIndex({ value: Math.round(index.value) });
+      },
+    });
+  });
+
+  useEffect(() => {
+    const scale = window.devicePixelRatio;
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+    const img = new Image();
+    img.src = images[index.value];
+    img.onload = () => {
+      canvas.width = img.width;
+      canvas.height = img.height;
+      ctx.drawImage(img, 0, 0);
+    };
+  }, [index]);
+  return (
+    <canvas
+      ref={canvasRef}
+      style={{ width: `${size}px`, height: `${size}px` }}
+      id="canvas"
+    ></canvas>
+  );
+};
+
+export default Canvas;
